78e4a517843dcf88a4db7866ffd1bd979fd4ac87
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2021-09-19 19:49:51 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -53,7 +53,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [Any Type](t.me/Benwolf), or [🇮🇳 Channel](t.me/Rex_Botz)."""
+There are ways of paying him; [Any Payment Method](t.me/Benwolf) , [🇮🇳 Channel](t.me/Rex_Botz)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
